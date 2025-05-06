f334910027e5610c8e71620a711ced007910011b
andrei.udriste <andrei.udriste@marel.com>
2025-05-06 13:23:35 +0200

    type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <array>
+
+std::array<std::string, 3> commands = {"echo", "exit", "type"};
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,14 +16,29 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    // Exit the loop if the user types "exit"
     if (input == "exit 0") {
-      break; // Exit the loop if the user types "exit"
+      break;
     }
 
+    // Simulate the echo command
     if (input.find("echo ") == 0) {
-      std::cout << input.substr(5) << "\n"; // Simulate the echo command
+      std::cout << input.substr(5) << "\n"; 
       continue;
     }
+
+    // Simulate the type command
+    if (input.find("type ") == 0) {
+      for (const auto& command : commands) {
+        if (input.substr(5) == command) {
+          std::cout << command << " is a shell builtin\n"; 
+          break;
+        }
+      }
+      std::cout << input.substr(5) << ": not found\n";
+      continue;
+    }
+    
     // Command not found
     std::cout << input << ": command not found" << std::endl;
   }
