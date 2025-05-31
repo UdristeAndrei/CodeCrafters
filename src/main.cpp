a3a0ba1d73e67335c1c85fc7f755d170ef0dd6cd
UdristeAndrei <36791521+UdristeAndrei@users.noreply.github.com>
2025-05-31 13:36:38 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -557,7 +557,7 @@ int main() {
 			// Check to see if you the user is trying to use an unknown command
 			UnknownCommand(commandData);
 			if (commandData.command == "cat"){
-				execvp("cat", {commandData.args.c_str()});
+				execlp("cat", commandData.args.c_str(), (char *) NULL);
 			}
 			
 			previousStdout = commandData.stdoutCmd; // Set the stdin for the next command
