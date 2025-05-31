2971d69855c0fc5574c40e5705235b781ab249b4
UdristeAndrei <36791521+UdristeAndrei@users.noreply.github.com>
2025-05-31 16:24:29 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -486,8 +486,8 @@ void UnknownCommand(CommandData& commandData) {
 
 				// // If the command is quoted, execute it with the arguments
 				// execvp(command_path.c_str(), argumentList);
-				system((command_path + " " + commandData.args).c_str());
-				exit(1); // Exit the child process after executing the command
+				system((originalCommand + " " + commandData.args).c_str());
+				_Exit() // Exit the child process after executing the command
 			}
 
 			// Parent: write previous command output to stdin of the child process
