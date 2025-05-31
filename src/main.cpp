a47a76bb8bc68e4c12b20f47c0646ccb04a69a9b
UdristeAndrei <36791521+UdristeAndrei@users.noreply.github.com>
2025-05-31 13:30:26 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -557,7 +557,7 @@ int main() {
 			// Check to see if you the user is trying to use an unknown command
 			UnknownCommand(commandData);
 			if (commandData.command == "cat"){
-				std::cout << commandData.stdoutCmd << "\n";
+				std::cout << commandData.stdoutCmd << "test\n";
 			}
 			
 			previousStdout = commandData.stdoutCmd; // Set the stdin for the next command
