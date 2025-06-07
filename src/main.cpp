8cf9f0a4d8b0f4a1f2044c392683004328f0cfbd
AndreiUdriste <udriste.andrei.98@gmail.com>
2025-06-08 00:38:18 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -509,12 +509,13 @@ void UnknownCommand(CommandData& commandData) {
 			while ((bytesRead = read(outpipe[0], buffer, sizeof(buffer) - 1)) > 0) {
 				buffer[bytesRead] = '\0'; // Null-terminate the string
 				output += buffer; // Append the output to the string
-				if (originalCommand == "tail") {
-					// If the command is "tail", print the output to stdout
-					std::cout << output;
-				}
 			}
 			close(outpipe[0]); // Close the read end of the pipe
+
+			if (originalCommand == "tail") {
+				// If the command is "tail", print the output to stdout
+				std::cout << output;
+			}
 			
 			// Store the output in the stdoutCmd
 			commandData.stdoutCmd = output;
